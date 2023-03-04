1cc61e9f430295564c55b0923888a3fcd4f8ed9e
Philip <muellers.philip@googlemail.com>
2023-03-04 14:55:41 +0100

    trying to resolve github pages issues with vue-router #5

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,45 +6,47 @@ import Simulation4 from "@/components/Simulation4.vue";
 import PageNotFound from "@/components/PageNotFound.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: SimulationPicker,
-    alias: '/simulationen'
-  },
-  {
-    path: '/sim1',
-    name: 'sim1',
-    component: Simulation1,
-    alias: '/messprozess'
-  },
-  {
-    path: '/sim2',
-    name: 'sim2',
-    component: Simulation1,
-    props: {
-      isSecond: true
+    {
+        path: '/',
+        name: 'home',
+        component: SimulationPicker,
+        alias: '/simulationen',
+        children: [
+            {
+                path: '/sim1',
+                name: 'sim1',
+                component: Simulation1,
+                alias: '/messprozess'
+            },
+        ]
     },
-    alias: '/statistischer_charakter'
-  },
-  {
-    path: '/sim3',
-    name: 'sim3',
-    component: Simulation3,
-    alias:'/verschraenkung'
-  },
-  {
-    path: '/sim4',
-    name: 'sim4',
-    component: Simulation4,
-    alias:'/dekohaerenzzeit'
-  },
-  { path: "/:pathMatch(.*)*", component: PageNotFound }
+    {
+        path: '/sim2',
+        name: 'sim2',
+        component: Simulation1,
+        props: {
+            isSecond: true
+        },
+        alias: '/statistischer_charakter'
+    },
+    {
+        path: '/sim3',
+        name: 'sim3',
+        component: Simulation3,
+        alias: '/verschraenkung'
+    },
+    {
+        path: '/sim4',
+        name: 'sim4',
+        component: Simulation4,
+        alias: '/dekohaerenzzeit'
+    },
+    {path: "/:pathMatch(.*)*", component: PageNotFound}
 ]
 
 const router = createRouter({
-  history: createWebHistory('/Qubits4Pupils'),
-  routes
+    history: createWebHistory('/Qubits4Pupils'),
+    routes
 })
 
 export default router
