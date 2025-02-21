ce0d888bf1929231835400a2999654b95c01bd07
Philip <muellers.philip@googlemail.com>
2025-02-21 19:47:34 +0100

    Adjusted public path for hosting via custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-lconst {defineConfig} = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
 const publicPath = process.env.NODE_ENV === "production" ? `/${repoName}/` : "/";
